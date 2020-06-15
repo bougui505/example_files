1a177b0e0a9b7180d8e5ae10639e10884fbac00d
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-06-15 17:12:14 +0200

    modified:   README.md 	new file:   generate_README.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # List of example input files useful for testing scripts
+.
+├── generate_README.sh
+├── MD_trajectories
+│   └── dcd
+│       ├── 2lj5.dcd
+│       └── 2lj5.pdb
+└── README.md
+
+2 directories, 4 files
---
+++ generate_README.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-06-15 17:11:45 (UTC+0200)
+
+echo "# List of example input files useful for testing scripts"
+tree
